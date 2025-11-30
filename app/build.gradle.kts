17d10125c33caf35580c44b820693bb99cec5e4c
shihao <eduwo479@gmail.com>
2025-12-01 00:47:25 +0800

    chore: 更新版本号到 v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.webtoapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.2"
 
         vectorDrawables {
             useSupportLibrary = true
